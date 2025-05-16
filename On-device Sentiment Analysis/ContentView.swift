3ecb48543a4046117664158b2601ac5aec34ff61
PratikshaMohadare <pratiksha.mohadare9@gmail.com>
2025-05-16 06:41:18 +0530

    Initial Commit

---
+++ On-device Sentiment Analysis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  On-device Sentiment Analysis
+//
+//  Created by Pratiksha on 16/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ On-device Sentiment Analysis/On_device_Sentiment_AnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_device_Sentiment_AnalysisApp.swift
+//  On-device Sentiment Analysis
+//
+//  Created by Pratiksha on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct On_device_Sentiment_AnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
